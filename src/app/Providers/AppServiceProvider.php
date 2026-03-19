2ec17f5b9cdcb8740c89200bf608f0a1e0618991
Ederson Dias Silva <edersondiassilva@gmail.com>
2026-03-19 16:34:33 -0500

    fix: force https on production

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +14,8 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        Paginator::useBootstrapFive();
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
